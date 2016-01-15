c150887166e6b132999f5763455cfffadb00247f
Cody Coyotee <cchoward@ufl.edu>
2016-01-15 14:47:47 -0500

    I have solved this.

--- R/exercise.R
+++ R/exercise.R
@@ -2,9 +2,11 @@
 #Replace the things that is of the form "__STUFF__" with actual content.
 #It might still need the quotes; it might not.
 
+library(ape)
+
 GetHowManyTrees_exercise <- function(ntaxa=10) {
 	#ape can compute this for us.
-	result <- "__A FUNCTION FROM APE TO DO THIS__(ntaxa)__"
+	result <- ace(ntaxa)
 	return(result)	
 }
 
@@ -12,6 +14,6 @@ GetAlignment_exercise <- function() {
 	#Let's see where mafft is installed
 	print(system("which mafft"))
 	data(woodmouse)
-	result <- mafft(" __PUT SOMETHING HERE TO ALIGN__" ) #hint, look up one line
+	result <- mafft("AGGATGCTACGATCAGTTAGCTAGCTACGT" ) #hint, look up one line
 	return(result)
 }
